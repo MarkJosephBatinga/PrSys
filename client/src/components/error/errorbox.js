bb1e25fa32b6d097b89493f334d44ae85eaaaa92
Mark Joseph Batinga <91181348+MarkJosephBatinga@users.noreply.github.com>
2023-03-21 15:54:43 +0800

    Update errorbox.js
    Added useNavigate Function

--- client/src/components/error/errorbox.js
+++ client/src/components/error/errorbox.js
@@ -2,8 +2,17 @@ import React from 'react'
 import * as EDesign from './error.style.js'
 import Image from 'mui-image'
 import NotFound from '../../images/404.svg'
+import { useNavigate } from "react-router-dom";
 
-export default function errorbox(props) {
+export default function Errorbox(props) {
+ const navigate = useNavigate();
+
+ function HandleClick(e) {
+    e.preventDefault()
+
+    navigate('/')
+ }
+
   return (
     <EDesign.ErrorStack>
         <EDesign.ImageBox>
@@ -12,7 +21,7 @@ export default function errorbox(props) {
        <EDesign.TextBox>
             <EDesign.ResultText>NO RESULTS FOUND</EDesign.ResultText>
             <EDesign.ContentText>Error: {props.error}</EDesign.ContentText>
-            <EDesign.NavButton>GO BACK</EDesign.NavButton>
+            <EDesign.NavButton onClick={HandleClick}>GO BACK</EDesign.NavButton>
        </EDesign.TextBox>
     </EDesign.ErrorStack>
   )
