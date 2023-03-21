e8d6b45dac2cd9f0524fa04cfe6ba6d131904773
Juanette Catalene Lictaoa <juanettecataleneb.lictaoa@gmail.com>
2023-03-21 11:22:27 +0800

    Create theme.js
    Added theme.js

---
+++ client/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette:{
+        primary:{
+            main: "#1C7293"
+        },
+        secondary:{
+            main: "#000000"
+        },
+        signBtn: {
+            main: "#1C7293"
+        },
+        white: {
+            main: "#FFFFFF"
+        }
+    }
+})
