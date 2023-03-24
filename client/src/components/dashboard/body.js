3f90d17e39bd4b4ef8589bb4bc19f5b07d72bda3
Juanette Catalene Lictaoa <juanettecataleneb.lictaoa@gmail.com>
2023-03-24 14:43:28 +0800

    Replaced body and lowerbody.style
    Replaced body.js in dashboard folder and lowerbody.style.js in dashboard.style folder

--- client/src/components/dashboard/body.js
+++ client/src/components/dashboard/body.js
@@ -1,9 +1,22 @@
 import React from "react";
 import * as UpperBodyDesign from './dashboard.style/upperbody.style'
-import * as LowerBodyDesign from './dashboard.style/lowerbody.style'
+import * as LBDesign from './dashboard.style/lowerbody.style'
 import * as TextDesign from "./dashboard.style/body.style";
 import { Stack } from "@mui/system";
+import { Table, TableBody, TableContainer,} from "@mui/material";
 
+//procNum, name, mode, status, date
+function createData(procNum, name, mode, status, date) {
+    return {procNum, name, mode, status, date};
+}
+
+const rows= [
+    createData('R1-2023-03-004', 'Procurement 1', 'Small Value Procurement', 'On-going', 'March 12, 2023'),
+    createData('R1-2023-03-005', 'Procurement 2', 'Small Value Procurement', 'On-going', 'March 13, 2023'),
+    createData('R1-2023-03-006', 'Procurement 3', 'Small Value Procurement', 'On-going', 'March 14, 2023'),
+    createData('R1-2023-03-007', 'Procurement 4', 'Small Value Procurement', 'On-going', 'March 15, 2023'),
+    createData('R1-2023-03-008', 'Procurement 5', 'Small Value Procurement', 'On-going', 'March 16, 2023'),
+];
 
 export default function Body(){
     return(
@@ -68,9 +81,32 @@ export default function Body(){
                 </UpperBodyDesign.UpperContentStack>
                 {/* END OF OUTER STACK */}
 
-             {/* START OF LOWER BODY    */}
-            <LowerBodyDesign.LowerBody>
-            </LowerBodyDesign.LowerBody>
+            {/* START OF LOWER BODY    */}
+            <LBDesign.BodyStack>
+                <LBDesign.BodyTitle>Procurement</LBDesign.BodyTitle>
+
+                <TableContainer>
+                    <Table>
+                        <TableBody>
+
+                            {rows.map((row) => (
+                                <LBDesign.Row key={row.procNum}>
+                                    <LBDesign.ProcNumCell>{row.procNum}</LBDesign.ProcNumCell>
+                                    <LBDesign.DetailCell>{row.name}</LBDesign.DetailCell>
+                                    <LBDesign.DetailCell>{row.mode}</LBDesign.DetailCell>
+                                    <LBDesign.StatusCell>
+                                        <LBDesign.StatusBox>
+                                            {row.status}
+                                        </LBDesign.StatusBox>
+                                    </LBDesign.StatusCell>
+                                    <LBDesign.DetailCell>{row.date}</LBDesign.DetailCell>
+                                </LBDesign.Row>
+                            ))}
+
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </LBDesign.BodyStack>
             {/* END OF LOWER BODY */}
         </Stack>
     )
--- client/src/components/dashboard/dashboard.style/lowerbody.style.js
+++ client/src/components/dashboard/dashboard.style/lowerbody.style.js
@@ -1,14 +1,74 @@
-import {
-    styled,
-    Stack,
-} from '@mui/material'
+import {styled, Stack, Typography, TableCell, TableRow, Box,} from '@mui/material'
 
 // LOWER BODY STYLE STARTS HERE
-export const LowerBody = styled(Stack)(({theme}) => ({
-    width: "100%",
-    marginTop: "20px",
+export const BodyStack = styled(Stack)(({theme}) => ({
+    // width: "100%",
+    padding: "1.5rem 1rem",
     display: "flex",
     justifyContent: 'flex-end',
-    height: '10vh',
-    backgroundColor: 'pink',
+    // backgroundColor: 'pink',
+}));
+
+export const BodyTitle = styled(Typography)(({theme}) => ({
+    fontFamily: 'Poppins, sans-serif',
+    fontSize: '1rem',
+    fontWeight:600,
+    [theme.breakpoints.up('lg')]: {
+        fontSize: '1rem',
+    },
+    [theme.breakpoints.up('xl')]: {
+        fontSize: '1.2rem',
+    },
+}));
+
+export const Row = styled(TableRow)(({theme}) => ({
+    borderBottom: '0.1rem solid #6C757D',
+    align: 'right'
+}));
+
+export const ProcNumCell = styled(TableCell)(({theme}) => ({
+    fontFamily: 'Poppins, sans-serif',
+    fontSize: '0.75rem',
+    fontWeight:600,
+    color: '#FF5959',
+    [theme.breakpoints.up('lg')]: {
+        fontSize: '0.75rem',
+    },
+    [theme.breakpoints.up('xl')]: {
+        fontSize: '1rem',
+    },
+}));
+
+export const DetailCell = styled(TableCell)(({theme}) => ({
+    fontFamily: 'Poppins, sans-serif',
+    fontSize: '0.75rem',
+    fontWeight:600,
+    color: '#000000',
+    [theme.breakpoints.up('lg')]: {
+        fontSize: '0.75rem',
+    },
+    [theme.breakpoints.up('xl')]: {
+        fontSize: '1rem',
+    },
+}));
+
+export const StatusCell = styled(TableCell)(({theme}) => ({
+    fontFamily: 'Poppins, sans-serif',
+    fontSize: '0.75rem',
+    fontWeight:600,
+    color: '#000000',
+    [theme.breakpoints.up('lg')]: {
+        fontSize: '0.75rem',
+    },
+    [theme.breakpoints.up('xl')]: {
+        fontSize: '1rem',
+    },
+    // backgroundColor: 'blue',
+}));
+
+export const StatusBox = styled(Box)(({theme}) => ({
+    backgroundColor: '#FACF5A',
+    padding: '0.1rem',
+    borderRadius: '1rem',
+    textAlign: 'center'
 }));
